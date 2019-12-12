4ff4f522ef370a669072a916374e9d674c648dc3
immma <immadiarabdul@gmail.com>
2019-12-12 15:39:07 +0700

    second commit

--- index.php
+++ index.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AWS Test Page</title>
+    <title>AWS TEST PAGE</title>
 </head>
 <body>
-    <h2>AWS Test Page</h2>
-
-    <h3>Hello World</h3>
+    <h2>AWS TEST PAGE - CODEPIPELINE</h2>
+    <h3>Hello world</h3>
 </body>
 </html>
